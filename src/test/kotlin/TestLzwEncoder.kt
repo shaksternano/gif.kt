a46e1f9ac59f6d7d8d9efc97ac3a2030149802f1
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2024-07-30 17:59:43 +0800

    test: remove `testLargeLzwEncode`

--- src/test/kotlin/TestLzwEncoder.kt
+++ src/test/kotlin/TestLzwEncoder.kt
@@ -46,29 +46,4 @@ class TestLzwEncoder {
         )
         assertEquals(expectedImageData, imageData)
     }
-
-    @Test
-    fun testLargeLzwEncode() {
-        val pixels = loadRgbPixels("media/restaurant.jpg")
-        val neuQuant = NeuQuant(
-            image = pixels,
-            samplingFactor = 10,
-        )
-        val colorTable = neuQuant.process()
-        val maxColors = colorTable.size / 3
-        val imageColorIndices = pixels.asList()
-            .chunked(3)
-            .map { (red, green, blue) ->
-                neuQuant.map(
-                    red.toInt() and 0xFF,
-                    green.toInt() and 0xFF,
-                    blue.toInt() and 0xFF,
-                ).toByte()
-            }
-            .toByteArray()
-        val buffer = Buffer()
-        buffer.writeGifImageData(imageColorIndices, maxColors)
-        val indexStream = buffer.readLzwIndexStream(maxColors)
-        assertEquals(imageColorIndices.asList(), indexStream)
-    }
 }

b3ee6dd0ce01bdf3a1d9e5d8f79b746c1b2b44f1
ShaksterNano <54268387+ShaksterNano@users.noreply.github.com>
2024-08-27 14:36:01 +0800

    docs: document `DisposalMethod`

--- src/main/kotlin/DisposalMethod.kt
+++ src/main/kotlin/DisposalMethod.kt
@@ -1,10 +1,36 @@
 package io.github.shaksternano.gifcodec
 
+/**
+ * Indicates the way in which a frame is to
+ * be treated after being displayed.
+ *
+ * Documentation taken from the
+ * [GIF89a specification](https://www.w3.org/Graphics/GIF/spec-gif89a.txt).
+ */
 enum class DisposalMethod(
     val id: Int,
 ) {
+    /**
+     * The decoder is not required to take
+     * any action.
+     */
     UNSPECIFIED(0),
+
+    /**
+     * The frame is to be left in place.
+     */
     DO_NOT_DISPOSE(1),
+
+    /**
+     * The area used by the frame must be
+     * restored to the background color.
+     */
     RESTORE_TO_BACKGROUND_COLOR(2),
+
+    /**
+     * The decoder is required to restore the
+     * area overwritten by the frame with what
+     * was there prior to rendering the frame.
+     */
     RESTORE_TO_PREVIOUS(3),
 }

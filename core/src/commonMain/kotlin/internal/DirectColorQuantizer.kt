f1d69f11bbfeca22d269116b793513510c5eeffb
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-06-24 22:09:15 +0100

    perf: remove use of intermediate collection operations

--- core/src/commonMain/kotlin/internal/DirectColorQuantizer.kt
+++ core/src/commonMain/kotlin/internal/DirectColorQuantizer.kt
@@ -17,18 +17,22 @@ internal data object DirectColorQuantizer : ColorQuantizer {
         private val colorIndices: Map<RGB, Int>
 
         init {
-            val distinctColors = rgb.asList()
-                .chunked(3)
-                .toSet()
-            colors = distinctColors.flatten()
-                .toByteArray()
-            colorIndices = distinctColors.mapIndexed { i, (red, green, blue) ->
-                RGB(
-                    red.toInt(),
-                    green.toInt(),
-                    blue.toInt(),
-                ) to i
-            }.toMap()
+            val distinctColors = hashSetOf<RGB>()
+            for (i in rgb.indices step 3) {
+                val red = rgb[i].toInt()
+                val green = rgb[i + 1].toInt()
+                val blue = rgb[i + 2].toInt()
+                distinctColors.add(RGB(red, green, blue))
+            }
+            colors = ByteArray(distinctColors.size * 3)
+            colorIndices = hashMapOf()
+            distinctColors.forEachIndexed { i, color ->
+                val index = i * 3
+                colors[index] = color.red.toByte()
+                colors[index + 1] = color.green.toByte()
+                colors[index + 2] = color.blue.toByte()
+                colorIndices[color] = i
+            }
         }
 
         override fun getColorIndex(red: Int, green: Int, blue: Int): Int {

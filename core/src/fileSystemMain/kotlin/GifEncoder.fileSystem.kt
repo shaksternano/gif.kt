3b5dbcc6490d3f3056a0f62e8da660282939724a
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-07-27 00:22:22 +0100

    docs: add kdoc to `GifEncoder` extension functions

--- core/src/fileSystemMain/kotlin/GifEncoder.fileSystem.kt
+++ core/src/fileSystemMain/kotlin/GifEncoder.fileSystem.kt
@@ -1,10 +1,21 @@
 package com.shakster.gifkt
 
+import kotlinx.io.IOException
 import kotlinx.io.buffered
 import kotlinx.io.files.Path
 import kotlinx.io.files.SystemFileSystem
 
+/**
+ * Creates a new [GifEncoderBuilder] for configuring and building a [GifEncoder].
+ *
+ * @param path The path of the file to write the GIF data to.
+ *
+ * @return A [GifEncoderBuilder] instance for configuring the encoder.
+ *
+ * @throws IOException If an I/O error occurs.
+ */
 @Suppress("EXTENSION_SHADOWED_BY_MEMBER")
+@Throws(IOException::class)
 fun GifEncoder.Companion.builder(path: Path): GifEncoderBuilder {
     return GifEncoderBuilder(SystemFileSystem.sink(path).buffered())
 }

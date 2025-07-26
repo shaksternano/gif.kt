2759c83befed25291dc9a9d649730d75c3580883
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-07-27 00:15:55 +0100

    fix: add missing android kdoc

--- core/src/androidMain/kotlin/GifEncoder.android.kt
+++ core/src/androidMain/kotlin/GifEncoder.android.kt
@@ -15,6 +15,58 @@ import kotlin.time.toKotlinDuration
 import java.nio.file.Path as JavaPath
 import java.time.Duration as JavaDuration
 
+/**
+ * A class for encoding GIF files.
+ * The encoder must be closed after use to ensure all data is written correctly.
+ *
+ * Basic usage:
+ * ```kotlin
+ * // Obtain a Sink to write the GIF data to
+ * val sink: Sink = ...
+ * val encoder = GifEncoder(sink)
+ *
+ * val argb: IntArray = ...
+ * val width: Int = ...
+ * val height: Int = ...
+ * val duration: Duration = ...
+ * encoder.writeFrame(argb, width, height, duration)
+ * encoder.close()
+ * ```
+ *
+ * @param sink The [Sink] to write the GIF data to.
+ *
+ * @param colorDifferenceTolerance The tolerance for color difference when performing transparency optimization.
+ * Set to -1 to disable transparency optimization.
+ *
+ * @param quantizedColorDifferenceTolerance The tolerance for color difference when performing transparency
+ * optimization after quantization. Set to -1 to disable post-quantization transparency optimization.
+ *
+ * @param loopCount The number of times the GIF should loop. Set to 0 for infinite looping.
+ * Set to -1 for no looping.
+ *
+ * @param maxColors The maximum number of colors in each frame, capped to [GIF_MAX_COLORS].
+ *
+ * @param colorQuantizer The [ColorQuantizer] to use for reducing the number of colors in each frame to [maxColors].
+ *
+ * @param colorSimilarityChecker The [ColorSimilarityChecker] to use for determining if two frames are similar
+ * enough to merge.
+ *
+ * @param comment An optional comment to include in the GIF comment block metadata.
+ *
+ * @param alphaFill The solid RGB color to use for filling in pixels with partial alpha transparency,
+ * as GIFs do not support partial transparency. Set to -1 to disable filling.
+ *
+ * @param cropTransparent Whether to crop the transparent pixels from the edges of each frame.
+ * This can reduce the size of the GIF by a small amount.
+ *
+ * @param minimumFrameDurationCentiseconds The minimum duration for each frame in centiseconds.
+ * Setting this to a value less than [GIF_MINIMUM_FRAME_DURATION_CENTISECONDS] can result in the GIF being played
+ * slower than expected on some GIF viewers.
+ *
+ * @param onFrameWritten A callback that is invoked after each frame is written,
+ * providing the number of frames written and the total duration written so far.
+ * This can be used to track progress or update a UI.
+ */
 actual class GifEncoder
 @JvmOverloads
 actual constructor(
@@ -50,6 +102,20 @@ actual constructor(
         onFrameWritten,
     )
 
+    /**
+     * Writes a single frame to the GIF.
+     *
+     * @param argb The ARGB pixel data for the frame,
+     * going row by row from top to bottom.
+     *
+     * @param width The width of the frame in pixels.
+     *
+     * @param height The height of the frame in pixels.
+     *
+     * @param duration The duration of the frame.
+     *
+     * @throws IOException If an I/O error occurs.
+     */
     @Throws(IOException::class)
     actual fun writeFrame(
         argb: IntArray,
@@ -65,6 +131,20 @@ actual constructor(
         )
     }
 
+    /**
+     * Writes a single frame to the GIF.
+     *
+     * @param argb The ARGB pixel data for the frame,
+     * going row by row from top to bottom.
+     *
+     * @param width The width of the frame in pixels.
+     *
+     * @param height The height of the frame in pixels.
+     *
+     * @param duration The duration of the frame.
+     *
+     * @throws IOException If an I/O error occurs.
+     */
     @Throws(IOException::class)
     fun writeFrame(
         argb: IntArray,
@@ -80,37 +160,85 @@ actual constructor(
         )
     }
 
+    /**
+     * Writes a single frame to the GIF.
+     *
+     * @param frame The [ImageFrame] containing the argb data, dimensions, and duration of the frame.
+     *
+     * @throws IOException If an I/O error occurs.
+     */
     @Throws(IOException::class)
     actual fun writeFrame(frame: ImageFrame) {
         baseEncoder.writeFrame(frame)
     }
 
+    /**
+     * Closes the encoder, ensuring all data is written.
+     * Closing the encoder also closes the underlying sink.
+     *
+     * @throws IOException If an I/O error occurs.
+     */
     @Throws(IOException::class)
     actual override fun close() {
         baseEncoder.close()
     }
 
     actual companion object {
+        /**
+         * Creates a new [GifEncoderBuilder] for configuring and building a [GifEncoder].
+         *
+         * @param sink The [Sink] to write the GIF data to.
+         *
+         * @return A [GifEncoderBuilder] instance for configuring the encoder.
+         */
         @JvmStatic
         actual fun builder(sink: Sink): GifEncoderBuilder {
             return GifEncoderBuilder(sink)
         }
 
+        /**
+         * Creates a new [GifEncoderBuilder] for configuring and building a [GifEncoder].
+         *
+         * @param outputStream The [OutputStream] to write the GIF data to.
+         *
+         * @return A [GifEncoderBuilder] instance for configuring the encoder.
+         */
         @JvmStatic
         fun builder(outputStream: OutputStream): GifEncoderBuilder {
             return GifEncoderBuilder(outputStream.asSink().buffered())
         }
 
+        /**
+         * Creates a new [GifEncoderBuilder] for configuring and building a [GifEncoder].
+         *
+         * @param path The path of the file to write the GIF data to.
+         *
+         * @return A [GifEncoderBuilder] instance for configuring the encoder.
+         */
         @JvmStatic
         fun builder(path: Path): GifEncoderBuilder {
             return GifEncoderBuilder(SystemFileSystem.sink(path).buffered())
         }
 
+        /**
+         * Creates a new [GifEncoderBuilder] for configuring and building a [GifEncoder].
+         *
+         * @param path The path of the file to write the GIF data to.
+         *
+         * @return A [GifEncoderBuilder] instance for configuring the encoder.
+         */
         @JvmStatic
         fun builder(path: JavaPath): GifEncoderBuilder {
             return GifEncoderBuilder(path.outputStream().asSink().buffered())
         }
 
+        /**
+         * Creates a new [GifEncoderBuilder] for configuring and building a [GifEncoder].
+         *
+         * @param file The file to write the GIF data to.
+         *
+         * @return A [GifEncoderBuilder] instance for configuring the encoder.
+         */
         @JvmStatic
         fun builder(file: File): GifEncoderBuilder {
             return GifEncoderBuilder(file.outputStream().asSink().buffered())

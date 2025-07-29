ba5bca720035aa36f7a41d0f9702d24354c3cf2d
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-07-29 22:02:02 +0100

    fix(test): set all fully transparent argb values to 0

--- core/src/jvmTest/kotlin/TestUtil.jvm.kt
+++ core/src/jvmTest/kotlin/TestUtil.jvm.kt
@@ -10,8 +10,26 @@ fun getResource(path: String): InputStream =
     TestUtil::class.java.classLoader.getResourceAsStream(path)
         ?: throw IllegalArgumentException("Resource not found: $path")
 
-fun loadImage(path: String): BufferedImage =
-    ImageIO.read(getResource(path))
+fun loadImage(path: String): BufferedImage {
+    val image = ImageIO.read(getResource(path))
+    val argbArray = image.rgb
+    val fixedAlpha = IntArray(argbArray.size) { i ->
+        val pixel = argbArray[i]
+        val rgb = RGB(pixel)
+        if (rgb.alpha == 0) {
+            0
+        } else {
+            pixel
+        }
+    }
+    val fixedImage = BufferedImage(
+        image.width,
+        image.height,
+        BufferedImage.TYPE_INT_ARGB,
+    )
+    fixedImage.rgb = fixedAlpha
+    return fixedImage
+}
 
 fun readGifFrames(resourcePath: String): List<ImageFrame> {
     val bytes = getResource(resourcePath).use {

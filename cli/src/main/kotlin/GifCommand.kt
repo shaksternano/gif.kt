772eeeb7b1c1555e80d1fe1bbf7ed99ca7bb3e4e
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-07-30 14:56:32 +0100

    feat: add more options to cli

--- cli/src/main/kotlin/GifCommand.kt
+++ cli/src/main/kotlin/GifCommand.kt
@@ -4,19 +4,17 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.terminal
 import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.help
-import com.github.ajalt.clikt.parameters.options.help
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.options.*
+import com.github.ajalt.clikt.parameters.types.boolean
+import com.github.ajalt.clikt.parameters.types.double
+import com.github.ajalt.clikt.parameters.types.int
 import com.github.ajalt.clikt.parameters.types.path
-import com.shakster.gifkt.ParallelGifEncoder
-import com.shakster.gifkt.use
+import com.shakster.gifkt.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.io.IOException
-import kotlinx.io.asSink
-import kotlinx.io.buffered
 import java.nio.file.Path
-import kotlin.io.path.outputStream
+import kotlin.time.Duration
 import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.microseconds
@@ -28,7 +26,7 @@ import kotlin.time.TimeSource
 
 object GifCommand : CliktCommand() {
 
-    private val input: Path by option("-i", "--input")
+    private val input: Path by option("--input", "-i")
         .path(
             mustExist = true,
             canBeDir = false,
@@ -36,6 +34,92 @@ object GifCommand : CliktCommand() {
         )
         .required()
         .help("The input file")
+
+    private val colorDifferenceTolerance: Double by option("--color-difference-tolerance")
+        .double()
+        .default(0.0)
+        .help("The tolerance for color difference used when performing transparency optimization. Set to -1 to disable transparency optimization.")
+
+    private val quantizedColorDifferenceTolerance: Double by option("--quantized-color-difference-tolerance")
+        .double()
+        .default(-1.0)
+        .help("The tolerance for color difference used when performing transparency optimization after quantization. Set to -1 to disable post-quantization transparency optimization.")
+
+    private val loopCount: Int by option("--loop-count")
+        .int()
+        .default(0)
+        .help("The number of times the GIF should loop. Set to 0 for infinite looping. Set to -1 for no looping.")
+
+    private val maxColors: Int by option("--max-colors")
+        .int()
+        .default(GIF_MAX_COLORS)
+        .help("he maximum number of colors in each frame. Must be between 1 and $GIF_MAX_COLORS.")
+        .validate {
+            require(it in 1..GIF_MAX_COLORS) {
+                "Max colors must be between 1 and $GIF_MAX_COLORS."
+            }
+        }
+
+    private val colorQuantizer: ColorQuantizer by option("--color-quantizer")
+        .convert {
+            when (it.lowercase()) {
+                "neuquant" -> ColorQuantizer.NEU_QUANT
+                "octree" -> ColorQuantizer.OCTREE
+                else -> {
+                    if (it.startsWith("neuquant-")) {
+                        val samplingFactor = it.substringAfter("-").toIntOrNull()
+                            ?: fail("Invalid neuquant sampling factor: $it")
+                        require(samplingFactor in 1..ColorQuantizer.NEU_QUANT_MAX_SAMPLING_FACTOR) {
+                            "NeuQuant sampling factor must be between 1 and ${ColorQuantizer.NEU_QUANT_MAX_SAMPLING_FACTOR}."
+                        }
+                        ColorQuantizer.neuQuant(samplingFactor)
+                    } else {
+                        fail("Unknown color quantizer: $it")
+                    }
+                }
+            }
+        }
+        .default(ColorQuantizer.NEU_QUANT)
+        .help("The color quantizer to use for reducing the number of colors in each frame to '--max-colors'. Available options: 'neuquant', 'neuquant-<samplingFactor>' where <samplingFactor> is an integer between 1 and ${ColorQuantizer.NEU_QUANT_MAX_SAMPLING_FACTOR}, or 'octree'.")
+
+    private val comment: String by option("--comment")
+        .help("An optional comment to include in the GIF comment block metadata.")
+        .default("")
+
+    private val alphaFill: Int by option("--alpha-fill")
+        .convert { option ->
+            option.toIntOrNull()
+                ?: option.toIntOrNull(16)
+                ?: fail("Invalid alpha fill color: $option. Must be an integer representing an RGB color.")
+        }
+        .default(-1)
+        .help("The solid RGB color to use for filling in pixels with partial alpha transparency, as GIFs do not support partial transparency. Set to -1 to disable filling.")
+
+    private val cropTransparent: Boolean by option("--crop-transparent")
+        .boolean()
+        .default(true)
+        .help("Whether to crop the transparent pixels from the edges of each frame. This can reduce the size of the GIF by a small amount.")
+
+    private val minimumFrameDurationCentiseconds: Int by option("--minimum-frame-duration-centiseconds")
+        .int()
+        .default(GIF_MINIMUM_FRAME_DURATION_CENTISECONDS)
+        .help("The minimum duration for each frame in centiseconds. Setting this to a value less than $GIF_MINIMUM_FRAME_DURATION_CENTISECONDS can result in the GIF being played slower than expected on some GIF viewers.")
+        .validate {
+            require(it > 0) {
+                "Minimum frame duration must be positive."
+            }
+        }
+
+    private val maxConcurrency: Int by option("--max-concurrency")
+        .int()
+        .default(Runtime.getRuntime().availableProcessors())
+        .help("The maximum number of frames that can be processed concurrently at the same time.")
+        .validate {
+            require(it > 0) {
+                "Max concurrency must be positive."
+            }
+        }
+
     private val output: Path by argument()
         .path()
         .help("The output file")
@@ -49,23 +133,38 @@ object GifCommand : CliktCommand() {
             return
         }.use { imageReader ->
             print("${renderProgressBar(0.0)} Processed 0/${imageReader.frameCount} frames, 0 FPS")
+            val builder = GifEncoder.builder(output)
+            builder.colorDifferenceTolerance = colorDifferenceTolerance
+            builder.quantizedColorDifferenceTolerance = quantizedColorDifferenceTolerance
+            builder.loopCount = loopCount
+            builder.maxColors = maxColors
+            builder.colorQuantizer = colorQuantizer
+            builder.comment = comment
+            builder.alphaFill = alphaFill
+            builder.cropTransparent = cropTransparent
+            builder.minimumFrameDurationCentiseconds = minimumFrameDurationCentiseconds
+            builder.maxConcurrency = maxConcurrency
+            builder.ioContext = Dispatchers.IO
+            val onFrameWrittenCallback = { framesWritten: Int, writtenDuration: Duration ->
+                val time = TimeSource.Monotonic.markNow()
+                val timeTaken = time - startTime
+                val fps = framesWritten / timeTaken.toDouble(DurationUnit.SECONDS)
+                val fpsFormatted = String.format("%.2f", fps)
+                val progress = framesWritten.toDouble() / imageReader.frameCount
+                print("\r${renderProgressBar(progress)} Processed $framesWritten/${imageReader.frameCount} frames, $fpsFormatted FPS")
+            }
             runBlocking {
-                ParallelGifEncoder(
-                    sink = output.outputStream().asSink().buffered(),
-                    colorDifferenceTolerance = 0.01,
-                    quantizedColorDifferenceTolerance = 0.02,
-                    maxConcurrency = Runtime.getRuntime().availableProcessors(),
-                    ioContext = Dispatchers.IO,
-                ) { framesWritten, writtenDuration ->
-                    val time = TimeSource.Monotonic.markNow()
-                    val timeTaken = time - startTime
-                    val fps = framesWritten / timeTaken.toDouble(DurationUnit.SECONDS)
-                    val fpsFormatted = String.format("%.2f", fps)
-                    val progress = framesWritten.toDouble() / imageReader.frameCount
-                    print("\r${renderProgressBar(progress)} Processed $framesWritten/${imageReader.frameCount} frames, $fpsFormatted FPS")
-                }.use { encoder ->
-                    imageReader.readFrames().forEach { imageFrame ->
-                        encoder.writeFrame(imageFrame)
+                if (maxConcurrency == 1) {
+                    builder.build(onFrameWrittenCallback).use { encoder ->
+                        imageReader.readFrames().forEach { imageFrame ->
+                            encoder.writeFrame(imageFrame)
+                        }
+                    }
+                } else {
+                    builder.buildParallel(onFrameWrittenCallback).use { encoder ->
+                        imageReader.readFrames().forEach { imageFrame ->
+                            encoder.writeFrame(imageFrame)
+                        }
                     }
                 }
             }

37bcdb78f6537f695f1987579cf4540c68af7f97
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-07-30 15:54:58 +0100

    docs: add kdoc to byte array source

--- core/src/commonMain/kotlin/ByteArraySource.kt
+++ core/src/commonMain/kotlin/ByteArraySource.kt
@@ -5,6 +5,13 @@ import kotlinx.io.Source
 import kotlinx.io.UnsafeIoApi
 import kotlinx.io.unsafe.UnsafeBufferOperations
 
+/**
+ * Creates a [Source] reading from this byte array.
+ *
+ * @param offset The offset in the byte array from which to start reading.
+ *
+ * @return A [Source] that reads from the byte array starting at the specified [offset].
+ */
 fun ByteArray.source(offset: Int = 0): Source {
     val buffer = Buffer()
     @OptIn(UnsafeIoApi::class)

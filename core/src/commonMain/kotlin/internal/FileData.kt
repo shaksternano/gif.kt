eae2e0281635d1761133379e5aed4ae077054446
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-07-30 16:20:59 +0100

    fix: map okio exceptions to kotlinx io

--- core/src/commonMain/kotlin/internal/FileData.kt
+++ core/src/commonMain/kotlin/internal/FileData.kt
@@ -1,6 +1,8 @@
 package com.shakster.gifkt.internal
 
 import com.shakster.gifkt.RandomAccessData
+import kotlinx.io.EOFException
+import kotlinx.io.IOException
 import kotlinx.io.RawSource
 import kotlinx.io.files.Path
 import kotlinx.io.okio.asKotlinxIoRawSource
@@ -22,20 +24,36 @@ internal class FileData(
         fileSystem,
     )
 
-    private val fileHandle: FileHandle = fileSystem.openReadOnly(path)
+    private val fileHandle: FileHandle = withOkio2KxIOExceptionMapping {
+        fileSystem.openReadOnly(path)
+    }
 
-    override fun source(offset: Long): RawSource {
+    override fun source(offset: Long): RawSource = withOkio2KxIOExceptionMapping {
         require(offset >= 0) {
             "offset ($offset) < 0"
         }
         return fileHandle.source(offset).asKotlinxIoRawSource()
     }
 
-    override fun close() {
+    override fun close() = withOkio2KxIOExceptionMapping {
         fileHandle.close()
     }
 
     override fun toString(): String {
         return "FileData(path=$path, fileSystem=$fileSystem)"
+    }
+
+    private inline fun <T> withOkio2KxIOExceptionMapping(block: () -> T): T {
+        try {
+            return block()
+        } catch (e: IOException) { // on JVM, kotlinx.io.IOException and okio.IOException are the same
+            throw e
+        } catch (e: EOFException) { // see above
+            throw e
+        } catch (e: okio.EOFException) {
+            throw EOFException(e.message)
+        } catch (e: okio.IOException) {
+            throw IOException(e.message, e)
+        }
     }
 }

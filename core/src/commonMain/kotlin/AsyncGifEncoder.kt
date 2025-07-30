adc62290628f631536cdfdafa9173da1d972f09b
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-07-30 18:59:15 +0100

    docs: add kdoc to `AsyncGifEncoder`

--- core/src/commonMain/kotlin/AsyncGifEncoder.kt
+++ core/src/commonMain/kotlin/AsyncGifEncoder.kt
@@ -15,6 +15,55 @@ import kotlin.concurrent.atomics.AtomicReference
 import kotlin.concurrent.atomics.ExperimentalAtomicApi
 import kotlin.time.Duration
 
+/**
+ * An abstract class for implementing asynchronous GIF encoders.
+ *
+ * Implementors of this class should provide implementations for [quantizeImage] and [encodeGifImage],
+ * and optionally override [wrapIo] and [onFrameWritten].
+ *
+ * @param sink The [Sink] to write the GIF data to.
+ *
+ * @param colorDifferenceTolerance The tolerance for color difference used by [colorSimilarityChecker]
+ * when performing transparency optimization.
+ *
+ * Set to -1 to disable transparency optimization.
+ *
+ * @param quantizedColorDifferenceTolerance The tolerance for color difference used by [colorSimilarityChecker]
+ * when performing transparency optimization after quantization.
+ *
+ * Set to -1 to disable post-quantization transparency optimization.
+ *
+ * @param loopCount The number of times the GIF should loop.
+ *
+ * Set to 0 for infinite looping.
+ *
+ * Set to -1 for no looping.
+ *
+ * @param maxColors The maximum number of colors in each frame, capped to [GIF_MAX_COLORS].
+ *
+ * @param colorQuantizer The [ColorQuantizer] to use for reducing the number of colors in each frame to [maxColors].
+ *
+ * @param colorSimilarityChecker The [ColorSimilarityChecker] to use for determining if two frames are similar
+ * enough to merge.
+ *
+ * @param comment An optional comment to include in the GIF comment block metadata.
+ *
+ * @param alphaFill The solid RGB color to use for filling in pixels with partial alpha transparency,
+ * as GIFs do not support partial transparency.
+ *
+ * Set to -1 to disable filling.
+ *
+ * @param cropTransparent Whether to crop the transparent pixels from the edges of each frame.
+ * This can reduce the size of the GIF by a small amount.
+ *
+ * @param minimumFrameDurationCentiseconds The minimum duration for each frame in centiseconds.
+ * Setting this to a value less than [GIF_MINIMUM_FRAME_DURATION_CENTISECONDS] can result in the GIF being played
+ * slower than expected on some GIF viewers.
+ *
+ * @param maxConcurrency The maximum number of frames that can be processed concurrently at the same time.
+ *
+ * @param coroutineScope The [CoroutineScope] in which the concurrent encoding operations will run.
+ */
 abstract class AsyncGifEncoder(
     private val sink: Sink,
     colorDifferenceTolerance: Double,
@@ -51,7 +100,14 @@ abstract class AsyncGifEncoder(
         minimumFrameDurationCentiseconds,
     )
 
+    /**
+     * Whether to perform transparency optimization after quantization.
+     */
     protected val optimizeQuantizedTransparency: Boolean = baseEncoder.optimizeQuantizedTransparency
+
+    /**
+     * The maximum number of colors in each frame.
+     */
     protected val maxColors: Int = baseEncoder.maxColors
 
     @OptIn(ExperimentalAtomicApi::class)
@@ -92,6 +148,25 @@ abstract class AsyncGifEncoder(
         }
     }
 
+    /**
+     * Writes a single frame to the GIF.
+     * The frame may be skipped if the [duration] is below [minimumFrameDurationCentiseconds],
+     * or if the frame is the same as or similar enough to the previous frame,
+     * determined by [colorDifferenceTolerance], [quantizedColorDifferenceTolerance],
+     * and [colorSimilarityChecker].
+     *
+     * @param argb The ARGB pixel data for the frame.
+     * Each element in the array represents a pixel in ARGB format,
+     * going row by row from top to bottom.
+     *
+     * @param width The width of the frame in pixels.
+     *
+     * @param height The height of the frame in pixels.
+     *
+     * @param duration The duration of the frame.
+     *
+     * @throws IOException If an I/O error occurs.
+     */
     suspend fun writeFrame(
         argb: IntArray,
         width: Int,
@@ -129,6 +204,17 @@ abstract class AsyncGifEncoder(
         }
     }
 
+    /**
+     * Writes a single frame to the GIF.
+     * The frame may be skipped if the duration is below [minimumFrameDurationCentiseconds],
+     * or if the frame is the same as or similar enough to the previous frame,
+     * determined by [colorDifferenceTolerance], [quantizedColorDifferenceTolerance],
+     * and [colorSimilarityChecker].
+     *
+     * @param frame The [ImageFrame] containing the argb data, dimensions, and duration of the frame.
+     *
+     * @throws IOException If an I/O error occurs.
+     */
     suspend fun writeFrame(frame: ImageFrame) {
         writeFrame(
             frame.argb,
@@ -156,6 +242,13 @@ abstract class AsyncGifEncoder(
         )
     }
 
+    /**
+     * Reduces the number of colors in the given image to a maximum of [maxColors] colors.
+     *
+     * @param input The [QuantizeInput] containing the image to quantize and other parameters.
+     *
+     * @return The [QuantizeOutput] containing the quantized image data and other parameters.
+     */
     protected abstract suspend fun quantizeImage(input: QuantizeInput): QuantizeOutput
 
     private suspend fun writeOrOptimizeGifImage(result: Result<QuantizeOutput>) {
@@ -213,6 +306,13 @@ abstract class AsyncGifEncoder(
         )
     }
 
+    /**
+     * Encodes the given image data to a GIF frame.
+     *
+     * @param input The [EncodeInput] containing the image data and other parameters.
+     *
+     * @return The [EncodeOutput] containing the encoded GIF frame and other parameters.
+     */
     protected abstract suspend fun encodeGifImage(input: EncodeInput): EncodeOutput
 
     private suspend fun transferToSink(result: Result<EncodeOutput>) {
@@ -229,10 +329,26 @@ abstract class AsyncGifEncoder(
         writtenFrameNotifications.send(output.duration)
     }
 
+    /**
+     * Wraps [sink] operations in order to make them suspending.
+     */
     protected open suspend fun wrapIo(block: () -> Unit) = block()
 
+    /**
+     * Called after each frame is written to the GIF.
+     *
+     * @param framesWritten The number of frames written so far.
+     *
+     * @param writtenDuration The total duration of all frames written so far.
+     */
     protected open suspend fun onFrameWritten(framesWritten: Int, writtenDuration: Duration) = Unit
 
+    /**
+     * Closes the encoder, ensuring all data is written.
+     * Closing the encoder also closes the underlying [sink].
+     *
+     * @throws IOException If an I/O error occurs.
+     */
     override suspend fun close() {
         var closeThrowable: Throwable? = null
         try {
@@ -282,6 +398,19 @@ abstract class AsyncGifEncoder(
         return IOException("Error while writing GIF frame", cause)
     }
 
+    /**
+     * Input data for the quantization process.
+     *
+     * @param optimizedImage The optimized image to quantize.
+     *
+     * @param originalImage The original image to use for reference.
+     *
+     * @param durationCentiseconds The duration of the frame in centiseconds.
+     *
+     * @param disposalMethod The disposal method to use for the frame.
+     *
+     * @param optimizedPreviousFrame Whether the previous frame was optimized.
+     */
     protected data class QuantizeInput(
         val optimizedImage: Image,
         val originalImage: Image,
@@ -290,6 +419,19 @@ abstract class AsyncGifEncoder(
         val optimizedPreviousFrame: Boolean,
     )
 
+    /**
+     * Output data for the quantization process.
+     *
+     * @param quantizedImageData The quantized image data.
+     *
+     * @param originalImage The original image used for reference.
+     *
+     * @param durationCentiseconds The duration of the frame in centiseconds.
+     *
+     * @param disposalMethod The disposal method used for the frame.
+     *
+     * @param optimizedPreviousFrame Whether the previous frame was optimized.
+     */
     protected data class QuantizeOutput(
         val quantizedImageData: QuantizedImageData,
         val originalImage: Image,
@@ -298,18 +440,47 @@ abstract class AsyncGifEncoder(
         val optimizedPreviousFrame: Boolean,
     )
 
+    /**
+     * Input data for the GIF encoding process.
+     *
+     * @param imageData The image data to encode.
+     *
+     * @param durationCentiseconds The duration of the frame in centiseconds.
+     *
+     * @param disposalMethod The disposal method to use for the frame.
+     */
     protected data class EncodeInput(
         val imageData: QuantizedImageData,
         val durationCentiseconds: Int,
         val disposalMethod: DisposalMethod,
     )
 
+    /**
+     * Output data for the GIF encoding process.
+     *
+     * @param source The [Source] containing the encoded GIF frame data.
+     *
+     * @param duration The duration of the frame.
+     */
     protected data class EncodeOutput(
         val source: Source,
         val duration: Duration,
     )
 }
 
+/**
+ * Quantizes the given image to a maximum number of colors.
+ *
+ * @param image The image to quantize.
+ *
+ * @param maxColors The maximum number of colors in the quantized image.
+ *
+ * @param quantizer The [ColorQuantizer] to use for quantization.
+ *
+ * @param forceTransparency Whether to reserve a color for transparency.
+ *
+ * @return The quantized image data.
+ */
 fun quantizeGifImage(
     image: Image,
     maxColors: Int,
@@ -324,6 +495,17 @@ fun quantizeGifImage(
     )
 }
 
+/**
+ * Encodes the given quantized image data to a GIF frame and writes it to the sink.
+ *
+ * @receiver The [Sink] to write the GIF data to.
+ *
+ * @param imageData The image data to write.
+ *
+ * @param durationCentiseconds The duration of the frame in centiseconds.
+ *
+ * @param disposalMethod The disposal method to use for the frame.
+ */
 fun Sink.writeGifImage(
     imageData: QuantizedImageData,
     durationCentiseconds: Int,

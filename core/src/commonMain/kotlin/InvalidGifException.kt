1761fd83fcf539fdd9da3c5984510e6f8747630a
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-07-30 18:27:42 +0100

    docs: add kdoc to `InvalidGifException`

--- core/src/commonMain/kotlin/InvalidGifException.kt
+++ core/src/commonMain/kotlin/InvalidGifException.kt
@@ -1,5 +1,11 @@
 package com.shakster.gifkt
 
+/**
+ * Exception thrown when a GIF file is invalid and cannot be decoded.
+ *
+ * @property message The detail message explaining the reason for the exception.
+ * @property cause The cause of the exception.
+ */
 class InvalidGifException(
     message: String,
     cause: Throwable? = null,

00718f189ab00521aaa1352e4bd777b6fe4b872d
ShaksterNano <54268387+shaksternano@users.noreply.github.com>
2025-09-14 16:50:48 +0100

    fix: don't use `MethodHandle` on android as it isn't supported at api level 24

---
+++ core/src/androidMain/kotlin/internal/ByteList.android.kt
@@ -0,0 +1,6 @@
+package com.shakster.gifkt.internal
+
+internal actual fun ByteArray.contentEquals(other: ByteArray, size: Int): Boolean {
+    return if (this === other) true
+    else contentEqualsCommon(this, other, size)
+}
